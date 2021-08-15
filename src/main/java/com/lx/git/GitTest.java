2633193b1c69001f3b830b988dd309ec2d8c45eb
Aiden9927 <88956250+Aiden9927@users.noreply.github.com>
2021-08-15 23:38:56 +0800

    pull commit

--- src/main/java/com/lx/git/GitTest.java
+++ src/main/java/com/lx/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git123");
+        System.out.println("pull test");
 
     }
 }
