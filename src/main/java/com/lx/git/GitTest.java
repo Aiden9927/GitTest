db54feea203f8970621de41ab37b3727af48b378
lx <liangxiao_14@outlook.com>
2021-08-15 22:18:35 +0800

    first commit

---
+++ src/main/java/com/lx/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lx.git;
+
+/**
+ * @author Aiden
+ * @create 2021-08-15 22:14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
